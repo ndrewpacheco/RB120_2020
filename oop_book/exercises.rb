57d3fa42fe09d11e3e2b258999fffb77f33c4021
Andrew  Pacheco <ndrewpacheco@gmail.com>
2020-05-22 15:47:02 -0400

    working on next chapter

--- oop_book/exercises.rb
+++ oop_book/exercises.rb
@@ -1,1 +1,31 @@
-sdf
+# How do we create an object in Ruby? Give an example of the creation of an object.
+
+# module Sup
+
+# end
+
+# class Objt
+#   include Sup
+# end
+
+
+# new_objt = Objt.new
+
+# What is a module? What is its purpose?
+# How do we use them with our classes?
+# Create a module for the class you created in exercise 1 and include it properly.
+
+# A module is a collection of behaviors that can be mixed in with various classes.
+
+###########################################
+
+# Create a class called MyCar.
+# When you initialize a new instance or object of the class,
+# allow the user to define some instance variables that tell us the year, color, and model
+#of the car.
+# Create an instance variable that is set to 0 during instantiation of the object to
+# track the current speed of the car as well.
+# Create instance methods that allow the car to speed up, brake, and shut the car off.
+
+
+
